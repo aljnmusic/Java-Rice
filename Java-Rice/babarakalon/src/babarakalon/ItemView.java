49910a4a4c6b4cd40765b88656d7e15a12e92b06
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-09-18 22:30:02 +0800

    feat: created a setter & setter

--- Java-Rice/babarakalon/src/babarakalon/ItemView.java
+++ Java-Rice/babarakalon/src/babarakalon/ItemView.java
@@ -7,8 +7,33 @@ package babarakalon;
 import java.util.Scanner;
 
 public class ItemView {
-    public int showMenu(){
 
+    private Scanner sc = new Scanner(System.in);
+
+    public int showMenu(){
+        System.out.println("Bakalon Manager");
+        System.out.println("[A] Add New Item\n[U] Update Item\n[D] Delete Item\n[S] Show all Items\n[Q] Quit");
+        System.out.print("Enter choice: ");
+        String option = sc.nextLine();
+
+        int optionCode;
+
+        switch (option){
+            case "A":
+            case "a":
+                optionCode = 1;
+                break;
+
+            case "U":
+            case  "u":
+                optionCode = 2;
+                break;
+
+            case "D":
+            case  "d":
+                optionCode = 3;
+                break;
+        }
     }
 
     public ItemModel addItemView(){
