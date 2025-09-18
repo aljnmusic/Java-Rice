75cbcd28cba8dda872f4a69fd608ebc75a2aada7
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-09-18 22:03:44 +0800

    initial commit on babarakalon

--- Java-Rice/babarakalon/src/babarakalon/Babarakalon.java
+++ Java-Rice/babarakalon/src/babarakalon/Babarakalon.java
@@ -4,11 +4,17 @@
  */
 package babarakalon;
 
+/**
+ *
+ * @author H
+ */
 public class Babarakalon {
 
-
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-
+        // TODO code application logic here
     }
     
 }
