b5d62c44012569e16d67cc61ae319f9fbacba668
aljnmusic <184954342+aljnmusic@users.noreply.github.com.>
2025-10-11 23:45:17 +0800

    Shape Inheritance

--- Java-Rice/shape_inheritance/src/shape_inheritance/Shape_inheritance.java
+++ Java-Rice/shape_inheritance/src/shape_inheritance/Shape_inheritance.java
@@ -23,7 +23,21 @@ public class Shape_inheritance {
         System.out.println("Units: " + triangle.getUnits());
         System.out.println("Is Filled: " + triangle.getisFilled());
         System.out.println("Area of Triangle: " + triangle.getArea() + " units squared");
-        System.out.println("Perimeter of Triangle: " + triangle.getPerimeter() + " units");
+        System.out.println("Perimeter of Triangle: " + triangle.getPerimeter() + " units\n");
+
+
+        Sphere sphere = new Sphere(3.0);
+        Cube cube = new Cube(2.0);
+        Tetrahedron tetrahedron = new Tetrahedron(5.0);
+
+        System.out.println("\nSPHERE");
+        System.out.println("Description: " + sphere.getDescription());
+        System.out.println("Surface area: " + sphere.calculateSurfaceArea());
+        System.out.println("Volume: " + sphere.calculateVolume() + "\n");
+
+        System.out.println("\nCube");
+        System.out.println("Description: " + cube.getDescription());
+
 
     }
     
